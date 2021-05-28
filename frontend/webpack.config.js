5342040a118f43dee8c723634701553d7f0f5162
Vitaly Budovski <vbudovski@gmail.com>
2021-05-28 20:31:02 +1000

    sys: Update dependencies

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -42,7 +42,9 @@ module.exports = {
           {
             loader: 'less-loader', // compiles Less to CSS
             options: {
-              javascriptEnabled: true
+              lessOptions: {
+                javascriptEnabled: true
+              }
             }
           },
         ],
